a84804e338d3040d3fc914175eb5fd9a066a89d6
Martin Hansen <code@martin-hansen.de>
2023-03-25 13:32:19 +0100

    fix: correct index for outOfBounds-errors

--- src/main/scala/io/github/mahh/kantangeneric/generic.scala
+++ src/main/scala/io/github/mahh/kantangeneric/generic.scala
@@ -5,13 +5,14 @@ import kantan.csv.DecodeResult
 import kantan.csv.RowDecoder
 
 import scala.compiletime.erasedValue
+import scala.compiletime.ops
 import scala.compiletime.summonInline
 import scala.deriving.*
 
 private type SliceSize = 25
 private inline def sliceSize = summon[ValueOf[SliceSize]].value
 
-inline private def tupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
+inline private def tupleRowDecoder[T <: Tuple, I <: Int]: RowDecoder[Tuple] =
   inline erasedValue[T] match
     case _: EmptyTuple =>
       RowDecoder.from(_ => DecodeResult.success(EmptyTuple))
@@ -20,18 +21,18 @@ inline private def tupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
         row.headOption.map { s =>
           for
             a <- summonInline[CellDecoder[t]].decode(s)
-            b <- tupleRowDecoder[ts].decode(row.drop(1))
+            b <- tupleRowDecoder[ts, ops.int.S[I]].decode(row.drop(1))
           yield a *: b
-        }.getOrElse(DecodeResult.outOfBounds(0))
+        }.getOrElse(DecodeResult.outOfBounds(summonInline[ValueOf[I]].value))
       }
 
-inline private def slicedTupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
+inline private def slicedTupleRowDecoder[T <: Tuple, I <: Int]: RowDecoder[Tuple] =
   inline erasedValue[T] match
     case _: EmptyTuple =>
       RowDecoder.from(_ => DecodeResult.success(EmptyTuple))
     case _: (t *: ts) =>
-      val takeDecoder = tupleRowDecoder[Tuple.Take[t *: ts, SliceSize]]
-      val dropDecoder = slicedTupleRowDecoder[Tuple.Drop[t *: ts, SliceSize]]
+      val takeDecoder = tupleRowDecoder[Tuple.Take[t *: ts, SliceSize], I]
+      val dropDecoder = slicedTupleRowDecoder[Tuple.Drop[t *: ts, SliceSize], ops.int.+[I, SliceSize]]
       RowDecoder.from { row =>
         val (takeRow, dropRow) = row.splitAt(sliceSize)
         for
@@ -41,4 +42,4 @@ inline private def slicedTupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
       }
 
 inline given derived[T](using p: Mirror.ProductOf[T]): RowDecoder[T] =
-  slicedTupleRowDecoder[p.MirroredElemTypes].map(p.fromProduct(_))
+  slicedTupleRowDecoder[p.MirroredElemTypes, 0].map(p.fromProduct(_))
--- src/test/scala/io/github/mahh/kantantest/GenericRowDecoderSuite.scala
+++ src/test/scala/io/github/mahh/kantantest/GenericRowDecoderSuite.scala
@@ -8,15 +8,17 @@ import munit.FunSuite
 
 class GenericRowDecoderSuite extends FunSuite {
 
+  case class CaseClass52(
+    a1: Int, b1: Int, c1: Int, d1: Int, e1: Int, f1: Int, g1: Int, h1: Int, i1: Int, j1: Int, k1: Int, l1: Int,
+    m1: Int, n1: Int, o1: Int, p1: Int, q1: Int, r1: Int, s1: Int, t1: Int, u1: Int, v1: Int, w1: Int, x1: Int,
+    y1: Int, z1: Int,
+    a2: Int, b2: Int, c2: Int, d2: Int, e2: Int, f2: Int, g2: Int, h2: Int, i2: Int, j2: Int, k2: Int, l2: Int,
+    m2: Int, n2: Int, o2: Int, p2: Int, q2: Int, r2: Int, s2: Int, t2: Int, u2: Int, v2: Int, w2: Int, x2: Int,
+    y2: Int, z2: Int
+  )
+
+
   test("derivation works for case classes with more than 32 fields") {
-    case class CaseClass52(
-      a1: Int, b1: Int, c1: Int, d1: Int, e1: Int, f1: Int, g1: Int, h1: Int, i1: Int, j1: Int, k1: Int, l1: Int,
-      m1: Int, n1: Int, o1: Int, p1: Int, q1: Int, r1: Int, s1: Int, t1: Int, u1: Int, v1: Int, w1: Int, x1: Int,
-      y1: Int, z1: Int,
-      a2: Int, b2: Int, c2: Int, d2: Int, e2: Int, f2: Int, g2: Int, h2: Int, i2: Int, j2: Int, k2: Int, l2: Int,
-      m2: Int, n2: Int, o2: Int, p2: Int, q2: Int, r2: Int, s2: Int, t2: Int, u2: Int, v2: Int, w2: Int, x2: Int,
-      y2: Int, z2: Int
-    )
     val decoder: RowDecoder[CaseClass52] = implicitly
     val decodeResult = decoder.decode((1 to 52).map(_.toString))
     val expected = Right(CaseClass52(
@@ -28,5 +30,26 @@ class GenericRowDecoderSuite extends FunSuite {
       50, 51, 52
     ))
     assertEquals(decodeResult, expected)
+  }
+
+  test("out of bounds is returned with correct index") {
+    val decoder: RowDecoder[CaseClass52] = derived
+    val oob = 42
+    val decodeResult = decoder.decode((0 until oob).map(_.toString))
+    val expected = DecodeResult.outOfBounds(oob)
+    assertEquals(decodeResult, expected)
+  }
+
+  test("error of a member-decoder is returned correctly") {
+    sealed trait Foo
+    val fooError = DecodeResult.typeError("Foo")
+
+    given CellDecoder[Foo] = CellDecoder.from(_ => fooError)
+
+    case class Bar(a: Int, b: Foo, c: String)
+    val decoder: RowDecoder[Bar] = derived
+
+    val decodeResult = decoder.decode(Seq("1", "foo", "some string"))
+    assertEquals(decodeResult, fooError)
   }
 }
