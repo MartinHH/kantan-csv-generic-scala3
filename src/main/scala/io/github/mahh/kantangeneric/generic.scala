e44377cc34f6e500ec382abe9eb219583cabd9c0
Martin Hansen <code@martin-hansen.de>
2023-03-25 09:22:29 +0100

    feat: support products with > maxInline fields

--- src/main/scala/io/github/mahh/kantangeneric/generic.scala
+++ src/main/scala/io/github/mahh/kantangeneric/generic.scala
@@ -8,6 +8,9 @@ import scala.compiletime.erasedValue
 import scala.compiletime.summonInline
 import scala.deriving.*
 
+private type SliceSize = 25
+private inline def sliceSize = summon[ValueOf[SliceSize]].value
+
 inline private def tupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
   inline erasedValue[T] match
     case _: EmptyTuple =>
@@ -15,12 +18,27 @@ inline private def tupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
     case _: (t *: ts) =>
       RowDecoder.from { row =>
         row.headOption.map { s =>
-          for {
+          for
             a <- summonInline[CellDecoder[t]].decode(s)
             b <- tupleRowDecoder[ts].decode(row.drop(1))
-          } yield a *: b
+          yield a *: b
         }.getOrElse(DecodeResult.outOfBounds(0))
       }
 
+inline private def slicedTupleRowDecoder[T <: Tuple]: RowDecoder[Tuple] =
+  inline erasedValue[T] match
+    case _: EmptyTuple =>
+      RowDecoder.from(_ => DecodeResult.success(EmptyTuple))
+    case _: (t *: ts) =>
+      val takeDecoder = tupleRowDecoder[Tuple.Take[t *: ts, SliceSize]]
+      val dropDecoder = slicedTupleRowDecoder[Tuple.Drop[t *: ts, SliceSize]]
+      RowDecoder.from { row =>
+        val (takeRow, dropRow) = row.splitAt(sliceSize)
+        for
+          a <- takeDecoder.decode(takeRow)
+          b <- dropDecoder.decode(dropRow)
+        yield a ++ b
+      }
+
 inline given derived[T](using p: Mirror.ProductOf[T]): RowDecoder[T] =
-  tupleRowDecoder[p.MirroredElemTypes].map(p.fromProduct(_))
+  slicedTupleRowDecoder[p.MirroredElemTypes].map(p.fromProduct(_))
--- src/test/scala/io/github/mahh/kantantest/GenericRowDecoderSuite.scala
+++ src/test/scala/io/github/mahh/kantantest/GenericRowDecoderSuite.scala
@@ -8,24 +8,25 @@ import munit.FunSuite
 
 class GenericRowDecoderSuite extends FunSuite {
 
-  test("derivation works for case classes with more than 22 members") {
-    // note that derivation for a case class with 31 parameters would fail with:
-    //   Maximal number of successive inlines(32) exceeded,
-    //   Maybe this is caused by a recursive inline method?
-    //   You can use -Xmax-inlines to change the limit.
-    // That is indeed cause by recursive inlining in io.github.mahh.kantangeneric.tupleRowDecoder
-    // and can be solved by incrementing that compiler-setting (to at least number_of_case_class_fields + 2).
-    case class CaseClass30(
+  test("derivation works for case classes with more than 32 fields") {
+    case class CaseClass52(
       a1: Int, b1: Int, c1: Int, d1: Int, e1: Int, f1: Int, g1: Int, h1: Int, i1: Int, j1: Int, k1: Int, l1: Int,
       m1: Int, n1: Int, o1: Int, p1: Int, q1: Int, r1: Int, s1: Int, t1: Int, u1: Int, v1: Int, w1: Int, x1: Int,
       y1: Int, z1: Int,
-      a2: Int, b2: Int, c2: Int, d2: Int
+      a2: Int, b2: Int, c2: Int, d2: Int, e2: Int, f2: Int, g2: Int, h2: Int, i2: Int, j2: Int, k2: Int, l2: Int,
+      m2: Int, n2: Int, o2: Int, p2: Int, q2: Int, r2: Int, s2: Int, t2: Int, u2: Int, v2: Int, w2: Int, x2: Int,
+      y2: Int, z2: Int
     )
-    val decoder: RowDecoder[CaseClass30] = implicitly
-    assertEquals(
-      decoder.decode((1 to 30).map(_.toString)),
-      Right(CaseClass30(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30))
-    )
+    val decoder: RowDecoder[CaseClass52] = implicitly
+    val decodeResult = decoder.decode((1 to 52).map(_.toString))
+    val expected = Right(CaseClass52(
+      1, 2, 3, 4, 5, 6, 7, 8, 9,
+      10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+      30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+      40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+      50, 51, 52
+    ))
+    assertEquals(decodeResult, expected)
   }
 }
